3700744d774774ca7ce6a0a255f900152d94842d
App Academy Student <tim.d.song@gmail.com>
2018-09-05 14:20:21 -0700

    Initialize URLShortener

---
+++ URLShortener/db/migrate/20180905210625_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.timestamps
+    end
+    
+    #TODO
+    add_index :users, [:username, :email], unique: true
+    add_index :users, :email, unique: true
+  end
+end
--- associations/app/models/course.rb
+++ associations/app/models/course.rb
@@ -1,2 +1,41 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint(8)        not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Course < ApplicationRecord
+  
+  has_many :enrollments,
+  primary_key: :id, 
+  foreign_key: :course_id, 
+  class_name: :Enrollment
+  
+  has_many :enrolled_students,
+  through: :enrollments,
+  source: :user
+  
+  # correct: returns prereq course
+  belongs_to :prerequisite,
+  primary_key: :id,
+  foreign_key: :prereq_id,
+  class_name: :Course
+  
+  # returns next course that this course is a prereq of?
+  
+  # has_many :prerequisite,
+  # primary_key: :id,
+  # foreign_key: :prereq_id,
+  # class_name: :Course
+  
+  belongs_to :instructor,
+  primary_key: :id,
+  foreign_key: :instructor_id, 
+  class_name: :User
 end
